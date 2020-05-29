c9323fe768b76109731a31892eddaebb791b3ae3
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2020-05-29 14:53:59 -0300

    filter search

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+
+function filterSearch() {
+    let txt1 = document.querySelector('#txt1');
+    let filterValue = txt1.value.toUpperCase();
+    let ul = document.querySelector('#menu');
+    let li = ul.getElementsByTagName('li');
+    let i;
+    for(i=0;i<li.length;i++) {
+        var a = li[i].getElementsByTagName('a')[0];
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
